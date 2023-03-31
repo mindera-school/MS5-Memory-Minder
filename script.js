a88a2f75a271f36918c9b520b51b126e16a02db2
gabiprosfriends <127973347+gabiprosfriends@users.noreply.github.com>
2023-03-31 12:25:44 +0100

    Add new commit

--- script.js
+++ script.js
@@ -4,26 +4,41 @@ let button = document.getElementsByTagName('button');
 const urls = ["https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Spaceship%20Color.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Camping.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Black%20Cat%20White%20Cat.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/California.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Cookie.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Alien.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Gamer.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Glases.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/MindMario.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Neon%20Blue.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Spaceship%20Color.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Camping.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Black%20Cat%20White%20Cat.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/California.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Cookie.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Alien.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Gamer.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Glases.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/MindMario.png?raw=true", "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Neon%20Blue.png?raw=true"];
 const compare = [];
 const div = document.getElementsByClassName('board');
+const buttonlist = [];
+let cardclick;
+let carigual;
 
-
-for (let i = 0; i < 20; i++) {
-	const button = document.createElement("button");
-	const image = document.createElement('img');
-	button.setAttribute("class", "cell");
-	carddeck.appendChild(button);
-	image.src = "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Slime.png?raw=true";
-	button.appendChild(image);
-}
-
-for (let i = 0; i < urls.length; i++) {
-	button[i].addEventListener("click", () => {
+function randomImages() {
+	for (let i = 0; i < urls.length; i++) {
 		for (let k = 0; k < 2; k++) {
 			const randomIndex = Math.floor(Math.random() * (i + 1));
 			[urls[i], urls[randomIndex]] = [urls[randomIndex], urls[i]];
 		}
+	}
 
-		for (let j = 0; j < button.length; j++) {
-			button[j].childNodes[0].src = urls[j];
-		}
+}
+
+for (let i = 0; i < 20; i++) {
+	const button = document.createElement("button");
+	const image = document.createElement('img');
+	button.setAttribute("image-index", i)
+	button.setAttribute("class", "cell");
+	buttonlist.push(button);
+	carddeck.appendChild(button);
+	image.src = "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Slime.png?raw=true";
+	button.appendChild(image);
+}
+randomImages()
+
+for (let i = 0; i < urls.length; i++) {
+	let button = buttonlist[i]
+	button.addEventListener("click", () => {
+
+		let index = button.getAttribute("image-index")
+		button.childNodes[0].src = urls[index];
+		setTimeout(() => {
+			button.childNodes[0].src = "https://github.com/mindera-school/minders/blob/master/src/assets/Minders/Slime.png?raw=true";
+		}, 5000);
+
 	})
 }
