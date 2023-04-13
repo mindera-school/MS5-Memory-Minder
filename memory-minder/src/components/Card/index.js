cd15597294974f4438232b21913cd0d39a032be9
Joao Rocha <rjoao0112@gmail.com>
2023-04-13 15:41:38 +0100

    Show Cards

---
+++ memory-minder/src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+function Card() {
+  return (<Conteiner>
+    
+  </Conteiner>);
+}
+
+const Conteiner = styled.div`
+  margin: 5px;
+  width: 150px;
+  height: 180px;
+  background-color: blue;
+  border-radius: 12px;
+`;
+
+export default Card;
