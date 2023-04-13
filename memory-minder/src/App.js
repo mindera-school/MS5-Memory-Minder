7926819375cf7634553485b1d6168c96fa621112
Adan0k <adansilvaoliveira@gmail.com>
2023-04-13 16:23:00 +0100

    add: randomize images

--- memory-minder/src/App.js
+++ memory-minder/src/App.js
@@ -1,6 +1,9 @@
 import Game from "./components/Game";
+import React, { useContext } from "react";
 
 function App() {
+  const appContext = useContext({});
+
   return (
     <div className="App">
       <Game></Game>
